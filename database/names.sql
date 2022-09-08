3ab9ba52bf0c66bfc66d4c578ee1bd8e12437314
RicardFabien <49694912+RicardFabien@users.noreply.github.com>
2022-09-08 23:14:44 +0200

    feat: added database auto populate

---
+++ database/names.sql
@@ -0,0 +1,24 @@
+create table NAMES (
+	id INT,
+	name VARCHAR(50)
+);
+insert into NAMES (id, name) values (1, 'Gardy');
+insert into NAMES (id, name) values (2, 'Marv');
+insert into NAMES (id, name) values (3, 'Eal');
+insert into NAMES (id, name) values (4, 'Renaud');
+insert into NAMES (id, name) values (5, 'Bertie');
+insert into NAMES (id, name) values (6, 'Tab');
+insert into NAMES (id, name) values (7, 'Shaine');
+insert into NAMES (id, name) values (8, 'Valery');
+insert into NAMES (id, name) values (9, 'Bastian');
+insert into NAMES (id, name) values (10, 'Janka');
+insert into NAMES (id, name) values (11, 'Sarina');
+insert into NAMES (id, name) values (12, 'Paton');
+insert into NAMES (id, name) values (13, 'Cosetta');
+insert into NAMES (id, name) values (14, 'Johnathan');
+insert into NAMES (id, name) values (15, 'Eileen');
+insert into NAMES (id, name) values (16, 'Clementius');
+insert into NAMES (id, name) values (17, 'Ray');
+insert into NAMES (id, name) values (18, 'Cherie');
+insert into NAMES (id, name) values (19, 'Khalil');
+insert into NAMES (id, name) values (20, 'Karlene');
